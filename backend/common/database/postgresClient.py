195fc4b53e4cc6d9d1760373eaea7256bf586881
Andres <andres.gutierrez.arcia@gmail.com>
2021-04-17 22:40:55 -0600

    adding env var

--- backend/common/database/postgresClient.py
+++ backend/common/database/postgresClient.py
@@ -1,5 +1,5 @@
 import psycopg2
-
+import os
 
 class PostgresClient:
 
@@ -7,7 +7,13 @@ class PostgresClient:
 
     def __init__(self):
         try:
-            self.connection = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='changeme'")
+            USER = os.getenv('API_USER')
+            db_name = os.environ.get('dbname')
+            db_user = os.environ.get('dbuser')
+            db_host = os.environ.get('dbhost')
+            db_password = os.environ.get('dbpassword')
+            # self.connection = psycopg2.connect(f"dbname='postgres' user='postgres' host='localhost' password='changeme'")
+            self.connection = psycopg2.connect(f"dbname='{db_name}' user='{db_user}' host='{db_host}' password='{db_password}'")
         except Exception as e:
             print(f"I am unable to connect to the database \n {e}")
         self.cursor = None
