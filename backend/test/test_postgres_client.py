5644040a4dfd84969cfc02e07c043e9a2f7f8319
Andrés Gutiérrez <andres.gutierrez.arcia@gmail.com>
2021-05-20 20:39:00 -0600

    Update test_postgres_client.py

--- backend/test/test_postgres_client.py
+++ backend/test/test_postgres_client.py
@@ -5,6 +5,7 @@ from common.database.postgresClient import PostgresClient
 
 class PostgresClientTest(unittest.TestCase):
 
+    @pytest.mark.skip(reason="no way of currently testing this")
     def test_connection_and_dummy_query(self):
         postgres_client = PostgresClient()
         query_result = postgres_client.test_query()
