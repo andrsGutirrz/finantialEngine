5c14fd6a7687ff5ff240e4122943f52a77dd7a7a
Andrés Gutiérrez <andres.gutierrez.arcia@gmail.com>
2021-05-20 20:37:56 -0600

    Update test_expense_engine.py

--- backend/test/test_expense_engine.py
+++ backend/test/test_expense_engine.py
@@ -6,6 +6,7 @@ from expenseEngine.expenseEngine import ExpenseEngine
 
 class PostgresClientTest(unittest.TestCase):
 
+    @pytest.mark.skip(reason="no way of currently testing this")
     def test_connection_and_dummy_query(self):
         postgres_client = PostgresClient()
         expense_engine = ExpenseEngine(db=postgres_client)
