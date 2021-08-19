45b40c63fcf3f247348fcd4c77cde48a6874716c
Andres <andres.gutierrez.arcia@gmail.com>
2021-08-18 21:35:29 -0600

    Disconnecting Postgres db

--- backend/test/test_expense_engine.py
+++ backend/test/test_expense_engine.py
@@ -6,9 +6,5 @@ from expenseEngine.expenseEngine import ExpenseEngine
 
 class PostgresClientTest(unittest.TestCase):
 
-#     @pytest.mark.skip(reason="no way of currently testing this")
-    def test_connection_and_dummy_query(self):
-#         postgres_client = PostgresClient()
-#         expense_engine = ExpenseEngine(db=postgres_client)
-#         expense_engine.get_all_expenses()
+    def test_dummy(self):
         self.assertFalse(False)
