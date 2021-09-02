0774fe9dc14a6a5f7579bae2610291b5a7da7f39
Andrés Gutiérrez <andres.gutierrez.arcia@gmail.com>
2021-09-01 19:56:45 -0600

    Update firebase_client.py

--- backend/common/firebase_client/firebase_client.py
+++ backend/common/firebase_client/firebase_client.py
@@ -8,7 +8,7 @@ def init():
     try:
         gcp_firebase_project_id = os.environ.get('GCP_FIREBASE_PROJECT_ID')
         cred = credentials.ApplicationDefault()
-
+        print(f"name {gcp_firebase_project_id}")
         firebase_admin.initialize_app(credential=cred, options={
             'databaseURL': f'https://{gcp_firebase_project_id}.firebaseio.com/'
         })
